1ca729e41ceaf58e35a8ee3b405e48ed71b67e77
Orestis Stefanis <68253122+magas3000@users.noreply.github.com>
2023-05-02 18:03:46 -0400

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
-        git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('https://github.com/orestisstefanis/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('magas3000')
+            node / gitConfigEmail('ostefan000@citymail.cuny.edu')
         }
     }
     triggers {
